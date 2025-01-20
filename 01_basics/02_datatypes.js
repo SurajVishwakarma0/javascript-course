52fa23a8c8b40c56114a5630b3a6df0f11d441af
SurajVishwakarma0 <137045365+SurajVishwakarma0@users.noreply.github.com>
2025-01-20 11:45:27 +0000

    2nd commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+let name_sk = "Suraj"
+let age = 21
+let study = true
+let placement = null
+let money
+/*
+datatypes:-
+string
+bigint
+boolean
+number
+null
+undefined
+symbol
+
+object
+*/
+console.log(typeof name_sk)
+console.log(typeof age)
+console.log(typeof study)
+console.log(typeof placement)
+console.log(typeof money)
