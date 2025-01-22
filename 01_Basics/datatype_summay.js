cdd5961eddbda3750553598b92a5e213e8ee7038
Suraj Vishwakarma <surajvishwakarmausa@gmail.com>
2025-01-22 15:39:48 +0530

    datatype summary

---
+++ 01_Basics/datatype_summay.js
@@ -0,0 +1,13 @@
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+let myObj= {
+    name: "Suraj",
+    age: 21
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
